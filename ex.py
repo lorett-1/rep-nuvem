81b44f918745e43961d96d87bae5f5db2758b75c
lorett-1 <lucas.lorenzo208@gmail.com>
2025-08-08 08:39:37 -0300

    teste

--- ex.py
+++ ex.py
@@ -1,2 +1,11 @@
-print("ola")
+acesso=input("qual seu cargo? [gerente,analista e estagiario?]: ")
+dia_da_semana=input("qual o dia da semana? [segunda,terça,quarta,quinta ou sexta?]: ")
+
+if acesso == "gerente" or acesso == "analista":
+    print("acesso concedido")
+elif acesso == "estagiario":
+    if dia_da_semana in ["segunda", "quarta","sexta"]:
+      print(f"acesso liberado para o dia {dia_da_semana}")
+    else:
+       print("acesso nao autorizado")
 
