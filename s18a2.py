7f2f763844be4cd9d43f8a213678292d10d15e06
lorett-1 <lucas.lorenzo208@gmail.com>
2025-08-29 07:21:01 -0300

    teste

--- s18a2.py
+++ s18a2.py
@@ -1,14 +1,12 @@
 import random
-
+tota_percorrido=0
 intervalo_n=int(input("qual o intervalo?[1, 1000]:"))
 while intervalo_n < 1 or intervalo_n > 1000:
     intervalo_n=int(input("qual o intervalo?[1, 1000]:"))
 for i in range(1,intervalo_n+1):
     tempo_t= random.randint(1,100)
     velocidade_v=random.randint(0,120)
-    
-intervalo_n=[1,1000]
-tempo_t=[1,100]
-velocidade_v=[0,120]
-total_percorrido= tempo_t * velocidade_v
-print
+print(f"intervalo {i}, tempo {tempo_t}, velocidade {velocidade_v}, distancia {tempo_t*velocidade_v}")
+distancia=tempo_t*velocidade_v
+total_percorrido=+ distancia
+print(f"o total percorrido em km é = {total_percorrido:3f}")
