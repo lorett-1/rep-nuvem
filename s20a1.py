840921eee55ea9904739b4752ebf05c925c4b792
lorett-1 <lucas.lorenzo208@gmail.com>
2025-09-11 09:03:36 -0300

    teste

--- s20a1.py
+++ s20a1.py
@@ -1,36 +1,6 @@
-
-def somar(a, b):
-    return a + b
- 
-def subtrair(a, b):
-    return a - b
- 
-def multiplicar(a, b):
-    return a * b
- 
-def dividir(a, b):
-    if b == 0:
-        raise ValueError("Divisão por zero não é permitida.")
-    return a / b
- 
+cont = 0
 while True:
-    escolha = input("Escolha a operação (+, -, *, /): ")
-    if escolha in ('+', '-', '*', '/'):
-        try:
-            num1 = float(input("Digite o primeiro número: "))
-            num2 = float(input("Digite o segundo número: "))
-            if escolha == '+':
-                print(f"{num1} + {num2} = {somar(num1, num2)}")
-            elif escolha == '-':
-                print(f"{num1} - {num2} = {subtrair(num1, num2)}")
-            elif escolha == '*':
-                print(f"{num1} * {num2} = {multiplicar(num1, num2)}")
-            elif escolha == '/':
-                print(f"{num1} / {num2} = {dividir(num1, num2)}")
-        except ValueError as e:
-            print(e)
-    continuar = input("Deseja continuar calculando?[sim,nao]: ").strip().lower()
-    if continuar != 'sim':
+    print(cont)
+    cont = cont + 1
+    if cont > 5:
         break
- 
- 
---
+++ s21a1.py
@@ -0,0 +1,14 @@
+acesso=input("qual seu cargo? [gerente,analista e estagiario?]: ").lower()
+dia_da_semana=input("qual o dia da semana? [segunda,terça,quarta,quinta ou sexta?]: ")
+
+if acesso == "gerente" or acesso == "analista":
+    print("acesso concedido")
+elif acesso == "estagiario":
+    if dia_da_semana in ["segunda", "quarta","sexta"]:
+      print(f"acesso liberado para o dia {dia_da_semana}")
+    else:
+       print("acesso nao autorizado")
+else:
+ print("nao encontrado")
+
+
